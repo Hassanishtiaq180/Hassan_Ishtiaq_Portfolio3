3a4c9915d1affcb20a6c6b42fc724c62d1b706e3
Hassanishtiaq180 <hassanishtiaq93@gmail.com>
2026-07-02 11:46:13 +0500

    refactor: update vanta color and show markdown sections instantly

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -187,7 +187,7 @@ function initHeroBackground() {
         minWidth: 200.0,
         scale: 1.0,
         scaleMobile: 1.0,
-        color: 0x1e3a8a,
+        color: 0x2dd4bf,
         backgroundAlpha: 0.0,
         points: 10.0,
         maxDistance: 22.0,
